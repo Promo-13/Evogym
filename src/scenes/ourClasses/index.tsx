0e5791284fca64126747e5bc5e8c8fc63d13af05
Promodh <160533725+Promo-13@users.noreply.github.com>
2024-11-29 10:52:56 +0530

    Update index.tsx

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -8,7 +8,6 @@ import image6 from "@/assets/image6.png";
 import { motion } from 'framer-motion';
 import HText from '@/shared/HText';
 import Class from "./Class";
-import { image } from 'framer-motion/client';
 
 const classes: Array<ClassType> = [
     {
